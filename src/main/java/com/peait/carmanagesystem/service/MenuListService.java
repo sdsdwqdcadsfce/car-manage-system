15b86e942556bf6f8d055bd62b659bbc3fc9cbf4
gouxuefeng <gouxuefeng@ysstech.com>
2020-06-22 17:59:33 +0800

    导出工具类 项目完结

--- src/main/java/com/peait/carmanagesystem/service/MenuListService.java
+++
@@ -1,8 +0,0 @@
-package com.peait.carmanagesystem.service;
-
-
-import com.peait.carmanagesystem.result.Result;
-
-public interface MenuListService {
-    Result queryEmnuList(String authorization);
-}
--- src/main/java/com/peait/carmanagesystem/service/ServiceListService.java
+++
@@ -1,7 +0,0 @@
-package com.peait.carmanagesystem.service;
-
-import com.peait.carmanagesystem.result.Result;
-
-public interface ServiceListService {
-    Result query();
-}
